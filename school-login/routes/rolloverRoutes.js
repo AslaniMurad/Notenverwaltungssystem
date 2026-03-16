48989f68de71b8a64bb11f3193afb81a49586fc2
GrasbergerSamuel <samuel.grasberger@htlwy.at>
2026-03-16 15:43:42 +0100

    schuljahreswechsel UI/UX enhanced

--- school-login/routes/rolloverRoutes.js
+++ school-login/routes/rolloverRoutes.js
@@ -7,7 +7,6 @@ const router = express.Router();
 router.use(requireAuth, requireRole("admin"));
 
 router.get("/rollover", rolloverController.showRolloverPage);
-router.post("/rollover/test", rolloverController.testRollover);
 router.post("/rollover/execute", rolloverController.executeRollover);
 
 module.exports = router;
