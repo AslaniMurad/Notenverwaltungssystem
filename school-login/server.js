9265d891bc9a0296f88848a15cd676836e898b04
AslaniMurad <murad.aslani@htlwy.at>
2026-04-23 09:43:34 +0200

    change startup

--- school-login/server.js
+++ school-login/server.js
@@ -497,5 +497,6 @@ if (require.main === module) {
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server läuft auf Port ${PORT}`);
+    console.log('PGHOST:', process.env.PGHOST);
   });
 }
